860330fd7604673d5aca11fdfc21c8b4f3f0e065
Nguyễn Thanh Thái <162539668+ThaiNguyen86@users.noreply.github.com>
2025-04-11 21:16:17 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,14 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+const connection = mysql.createConnection({
+    host: 'project1-taskmanager-db.cv86akkwwra1.ap-southeast-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Thai668084',
+    database: 'task_manager_db'
+});
+connection.connect();
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
